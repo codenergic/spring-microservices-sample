18f909951525ac67a4c7c7fa9584e12cab4f2c67
Dias Nurul Arifin <dias.arifin@gmail.com>
2018-09-28 16:50:41 +0700

    remote token service already support eureka

--- simple-service-gateway-zuul/src/main/java/org/codenergic/sample/gateway/config/OAuth2ResourceServerConfig.java
+++ simple-service-gateway-zuul/src/main/java/org/codenergic/sample/gateway/config/OAuth2ResourceServerConfig.java
@@ -1,7 +1,8 @@
 package org.codenergic.sample.gateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -9,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableResourceServer
@@ -22,14 +25,20 @@ public class OAuth2ResourceServerConfig
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(SpringClientFactory springClientFactory) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new RibbonClientHttpRequestFactory(springClientFactory));
+        return restTemplate;
+    }
+
     @Primary
     @Bean
-    public EurekaTokenServices tokenServices(DiscoveryClient discoveryClient,
-            @Value("${authorization.server.name}") String serviceName) {
-        final EurekaTokenServices tokenService = new EurekaTokenServices();
-        tokenService.setCheckTokenPath("/oauth/check_token");
-        tokenService.setDiscoveryClient(discoveryClient);
-        tokenService.setServiceName(serviceName);
+    public RemoteTokenServices tokenServices(RestTemplate restTemplate) {
+        final RemoteTokenServices tokenService = new RemoteTokenServices();
+        tokenService.setRestTemplate(restTemplate);
+        tokenService.setCheckTokenEndpointUrl("http://AuthorizationServer/oauth/check_token");
         tokenService.setClientId("clientIdPassword");
         tokenService.setClientSecret("secret");
         return tokenService;
