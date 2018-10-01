0e1e32ee03c503c4b7fd21c99f19b06672114915
Dias Nurul Arifin <dias.arifin@gmail.com>
2018-10-01 09:23:14 +0700

    delete wrong gateway module

--- simple-service-gateway/src/main/java/org/codenergic/sample/gateway/ServiceGatewayApplication.java
+++ simple-service-gateway/src/main/java/org/codenergic/sample/gateway/ServiceGatewayApplication.java
@@ -2,14 +2,12 @@ package org.codenergic.sample.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableCircuitBreaker
 @EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
-@EnableFeignClients
 public class ServiceGatewayApplication {
 
     public static void main(String[] args) {
